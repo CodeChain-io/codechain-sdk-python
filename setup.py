fb8d74c5962b1cca6f849215b564202340dc4fb1
GeunWoo Kim <posgnu@kodebox.io>
2019-07-24 16:17:46 +0800

    Write setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import io
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "codechain", "__version__.py"), "r", "utf-8") as f:
+    exec(f.read(), about)
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+packages = ["codechain"]
+
+requires = ["rlp"]
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    url=about["__url__"],
+    project_urls={
+        "Documentation": "",
+        "Code": "https://github.com/CodeChain-io/codechain-sdk-python",
+        "Issue tracker": "https://github.com/CodeChain-io/codechain-sdk-python/issues",
+    },
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    description=about["__description__"],
+    long_description=readme,
+    packages=packages,
+    package_dir={"codechain": "codechain"},
+    license=about["__license__"],
+    install_requires=requires,
+    extras_require={"dev": ["pytest"]},
+)
