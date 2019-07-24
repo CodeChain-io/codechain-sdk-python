89b93efd09bdff433d81e07420a34687ee9e58ad
Geunwoo Kim <posgnu@gmail.com>
2019-07-25 03:33:04 +0900

    Make files and directory for crypto lib

--- codechain/primitives/__init__.py
+++ codechain/primitives/__init__.py
@@ -12,14 +12,5 @@ from .UnsignedInteger import U64
 # Pythons
 # -------
 
-# Syntax sugar.
-_ver = sys.version_info
-
-#: Python 2.x?
-is_py2 = _ver[0] == 2
-
-#: Python 3.x?
-is_py3 = _ver[0] == 3
-
-if is_py2:
-    raise ValueError("Please use python3")
+if sys.version_info < (3, 6):
+    raise ValueError("Please use >=python3.6")
