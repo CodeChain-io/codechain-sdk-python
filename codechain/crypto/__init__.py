98d9e95429cc90b57bfaab6ed570d84ff1eda00b
Geunwoo Kim <posgnu@gmail.com>
2019-07-25 03:51:47 +0900

    Add hash functions

--- codechain/crypto/__init__.py
+++ codechain/crypto/__init__.py
@@ -1,5 +1,13 @@
 import sys
 
+from .hash import blake128
+from .hash import blake128_with_key
+from .hash import blake160
+from .hash import blake160_with_key
+from .hash import blake256
+from .hash import blake256_with_key
+from .hash import ripemd160
+
 # -------
 # Pythons
 # -------
