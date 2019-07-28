38ee7ac0eebb45866bc6671416e2aa82f7b3ef11
Geunwoo Kim <posgnu@gmail.com>
2019-07-29 03:09:22 +0900

    Make the codechain package as a normal package

---
+++ codechain/__init__.py
@@ -0,0 +1,20 @@
+import sys
+
+from .crypto import *
+from .primitives import *
+
+# -------
+# Pythons
+# -------
+
+# Syntax sugar.
+_ver = sys.version_info
+
+#: Python 2.x?
+is_py2 = _ver[0] == 2
+
+#: Python 3.x?
+is_py3 = _ver[0] == 3
+
+if is_py2:
+    raise ValueError("Please use python3")
--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-import codechain
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import codechain
