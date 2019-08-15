ea6e58eef017c1f6bc01c97740e187a8acbdb775
Geunwoo Kim <posgnu@gmail.com>
2019-08-16 01:52:31 +0900

    Add tests for ecdsa

---
+++ tests/test_ecdsa.py
@@ -0,0 +1,16 @@
+import pytest
+
+from codechain.crypto import generate_private_key
+from codechain.crypto import get_public_from_private
+from codechain.crypto import recover_ecdsa
+from codechain.crypto import sign_ecdsa
+from codechain.crypto import verify_ecdsa
+
+
+def test_sign_verify():
+    message = bytes(32)
+    priv = generate_private_key()
+    pub = get_public_from_private(priv)
+    sig = sign_ecdsa(message, priv)
+
+    assert verify_ecdsa(message, sig, pub)
