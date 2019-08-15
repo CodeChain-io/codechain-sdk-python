889413f26d9cbc219b929093aab817b9a0b559ae
Geunwoo Kim <posgnu@gmail.com>
2019-08-16 01:51:39 +0900

    Implement key generation functions

---
+++ codechain/crypto/key.py
@@ -0,0 +1,15 @@
+from coincurve import PrivateKey, PublicKey
+# Generates a private key.
+
+
+def generate_private_key():
+    privkey = PrivateKey()
+    return bytes(bytearray.fromhex(privkey.to_hex())
+
+
+# Gets public key from private key.
+
+
+def get_public_from_private(priv: bytes):
+    privkey=PrivateKey(priv)
+    return privkey.public_key.format(compressed=False)[1:]
