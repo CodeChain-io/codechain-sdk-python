cc310f0b422f4026a06678137198cc102e83ebbe
Geunwoo Kim <posgnu@gmail.com>
2019-08-27 00:48:53 +0900

    Add ecdsa test cases

--- tests/test_ecdsa.py
+++ tests/test_ecdsa.py
@@ -1,5 +1,4 @@
 import pytest
-from coincurve.utils import der_to_pem
 
 from codechain.crypto import generate_private_key
 from codechain.crypto import get_public_from_private
@@ -15,3 +14,12 @@ def test_sign_verify():
     sig = sign_ecdsa(message, priv)
 
     assert verify_ecdsa(message, sig, pub)
+
+
+def test_sign_recover():
+    message = bytes(32)
+    priv = generate_private_key()
+    pub = get_public_from_private(priv)
+    sig = sign_ecdsa(message, priv)
+
+    assert recover_ecdsa(message, sig) == pub
