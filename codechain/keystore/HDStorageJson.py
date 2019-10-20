2b32513b1bb526455995f83948fde92faf86c06c
Geunwoo Kim <posgnu@kodebox.io>
2019-10-21 06:00:56 +0900

    Add json decoding for storage

--- codechain/keystore/HDStorageJson.py
+++ codechain/keystore/HDStorageJson.py
@@ -6,6 +6,8 @@ from Crypto.Cipher import AES
 from Crypto.Util import Counter
 
 from ..crypto import blake256
+from .Errors import ErrorCode
+from .Errors import KeystoreError
 from .Pbkdf2 import pbkdf2
 
 
@@ -22,11 +24,7 @@ def encode(seed: str, passphrase: str, meta: str):
         "prf": "hmac-sha256",
     }
     derived_key = pbkdf2(
-        passphrase.encode("utf-8"),
-        salt,
-        kdf_params.get("c"),
-        kdf_params.get("dklen"),
-        "sha256",
+        passphrase.encode("utf-8"), salt, kdf_params["c"], kdf_params["dklen"], "sha256"
     )
     ctr = Counter.new(128, initial_value=int.from_bytes(iv, byteorder="big"))
     cipher = AES.new(derived_key[:16], AES.MODE_CTR, counter=ctr)
@@ -47,3 +45,33 @@ def encode(seed: str, passphrase: str, meta: str):
         "seedHash": binascii.hexlify(seed_hash).decode("ascii"),
         "meta": meta,
     }
+
+
+def decode(json, passphrase: str) -> str:
+    kdfparams = json["crypto"]["kdfparams"]
+    derivedKey = pbkdf2(
+        passphrase.encode("utf-8"),
+        bytes.fromhex(kdfparams["salt"]),
+        kdfparams["c"],
+        kdfparams["dklen"],
+        "sha256",
+    )
+    ciphertext = bytes.fromhex(json["crypto"]["ciphertext"])
+    mac = blake256(derivedKey[16:32] + ciphertext)
+
+    if mac != json["crypto"]["mac"]:
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    if json["crypto"]["cipher"] != "aes-128-ctr":
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    iv = int(json["crypto"]["cipherparams"]["iv"], 16)
+    ctr = Counter.new(128, initial_value=iv)
+    decipher = AES.new(derivedKey[0:16], AES.MODE_CTR, counter=ctr)
+    seed = decipher.decrypt(ciphertext)
+
+    seed_hash = binascii.hexlify(blake256(seed)).decode("ascii")
+    if seed_hash != json["seedHash"]:
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    return binascii.hexlify(seed).decode("ascii")
--- codechain/keystore/__init__.py
+++ codechain/keystore/__init__.py
@@ -1,6 +1,12 @@
 import sys
 
+from .HDStorageJson import decode as hd_decode
+from .HDStorageJson import encode as hd_encode
+from .KeyType import get_table_name
+from .KeyType import KeyType
 from .Pbkdf2 import pbkdf2
+from .StorageJson import decode
+from .StorageJson import encode
 
 # -------
 # Pythons
