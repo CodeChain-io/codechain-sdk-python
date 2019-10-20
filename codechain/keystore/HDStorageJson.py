f9c4ea2e9a3ec86b135909ab0a5f35c7ebb098b0
Geunwoo Kim <posgnu@kodebox.io>
2019-10-21 06:00:56 +0900

    Add json decoding for storage

--- codechain/keystore/HDStorageJson.py
+++ codechain/keystore/HDStorageJson.py
@@ -3,6 +3,7 @@ import os
 import uuid
 
 from Crypto.Cipher import AES
+from Crypto.Util import Counter
 
 from ..crypto import blake256
 from ..crypto import get_public_from_private
@@ -47,3 +48,33 @@ def encode(seed: str, passphrase: str, meta: str):
         "seedHash": seed_hash,
         "meta": meta,
     }
+
+
+def decode(json, passphrase: str) -> str:
+    kdfparams = json["crypto"]["kdfparams"]
+    derivedKey = pbkdf2(
+        passphrase.encode("utf-8"),
+        bytes.fromhex(kdfparams["salt"]),
+        kdfparams["c"],
+        kdfparams["dklen"],
+        "sha256",
+    )
+    ciphertext = bytes.fromhex(json["crypto"]["ciphertext"])
+    mac = blake256(derivedKey[16:32] + ciphertext)
+
+    if mac != json["crypto"]["mac"]:
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    if json["crypto"]["cipher"] != "aes-128-ctr":
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    iv = int(json["crypto"]["cipherparams"]["iv"], 16)
+    ctr = Counter.new(128, initial_value=iv)
+    decipher = AES.new(derivedKey[0:16], AES.MODE_CTR, counter=ctr)
+    seed = decipher.decrypt(ciphertext)
+
+    seed_hash = binascii.hexlify(blake256(seed)).decode("ascii")
+    if seed_hash != json["seedHash"]:
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    return binascii.hexlify(seed).decode("ascii")
--- codechain/keystore/StorageJson.py
+++ codechain/keystore/StorageJson.py
@@ -3,6 +3,7 @@ import os
 import uuid
 
 from Crypto.Cipher import AES
+from Crypto.Util import Counter
 
 from ..crypto import blake256
 from ..crypto import get_public_from_private
@@ -48,3 +49,29 @@ def encode(private_key: str, key_type: KeyType, passphrase: str, meta: str):
         "address": address,
         "meta": meta,
     }
+
+
+def decode(json, passphrase: str) -> str:
+    kdfparams = json["crypto"]["kdfparams"]
+    derivedKey = pbkdf2(
+        passphrase.encode("utf-8"),
+        bytes.fromhex(kdfparams["salt"]),
+        kdfparams["c"],
+        kdfparams["dklen"],
+        "sha256",
+    )
+    ciphertext = bytes.fromhex(json["crypto"]["ciphertext"])
+    mac = blake256(derivedKey[16:32] + ciphertext)
+
+    if mac != json["crypto"]["mac"]:
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    if json["crypto"]["cipher"] != "aes-128-ctr":
+        raise KeystoreError(ErrorCode.DECRYPTIONFAILED)
+
+    iv = int(json["crypto"]["cipherparams"]["iv"], 16)
+    ctr = Counter.new(128, initial_value=iv)
+    decipher = AES.new(derivedKey[0:16], AES.MODE_CTR, counter=ctr)
+    private_key = decipher.decrypt(ciphertext)
+
+    return binascii.hexlify(private_key).decode("ascii")
