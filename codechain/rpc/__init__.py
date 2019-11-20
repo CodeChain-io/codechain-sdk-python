ad0267be094d27e0b7681692fb41c78e4a769935
GNu <kgw6602@postech.ac.kr>
2019-11-20 15:51:11 +0900

    Implement rpc class

--- codechain/rpc/__init__.py
+++ codechain/rpc/__init__.py
@@ -1,5 +1,7 @@
 import sys
 
+from .rpc import Rpc
+
 # -------
 # Pythons
 # -------
--- codechain/rpc/rpc.py
+++ codechain/rpc/rpc.py
@@ -1,10 +1,25 @@
 from jsonrpcclient.clients.http_client import HTTPClient
 from jsonrpcclient.requests import Request
 
+from .account import Account
+from .chain import Chain
+from .devel import Devel
+from .engine import Engine
+from .mempool import Mempool
+from .net import Net
+
 
 class Rpc:
     def __init__(self, node: str, devel: bool):
         self.client = HTTPClient(node)
+
+        self.account = Account(self.client)
+        self.chain = Chain(self.client)
+        self.engine = Engine(self.client)
+        self.mempool = Mempool(self.client)
+        self.net = Net(self.client)
+        if devel:
+            self.devel = Devel(self.client)
 
     def ping(self):
         payload = Request("ping")
