717b2ea89f91543199f2ff7f1afede666800259d
GNu <kgw6602@postech.ac.kr>
2019-11-20 15:48:31 +0900

    Implement account rpc

---
+++ codechain/rpc/account.py
@@ -0,0 +1,60 @@
+from jsonrpcclient.requests import Request
+
+
+class Account:
+    def __init__(self, client):
+        self.client = client
+
+    def get_list(self):
+        payload = Request("account_getList")
+        response = self.client.send(payload)
+
+        return response.result
+
+    def create(self, passphrase):
+        payload = Request("account_create", passphrase=passphrase)
+        response = self.client.send(payload)
+
+        return response.result
+
+    def import_raw(self, secret, passphrase):
+        payload = Request("account_importRaw", secret=secret, passphrase=passphrase)
+        response = self.client.send(payload)
+
+        return response.result
+
+    def unlock(self, account, passphrase):
+        payload = Request("account_unlock", account=account, passphrase=passphrase)
+        response = self.client.send(payload)
+
+        return response.result
+
+    def sign(self, message, account, passphrase):
+        payload = Request(
+            "account_sign", message=message, account=account, passphrase=passphrase
+        )
+        response = self.client.send(payload)
+
+        return response.result
+
+    def send_transaction(self, transaction, account, passphrase):
+        payload = Request(
+            "account_sendTransaction",
+            transaction=transaction,
+            account=account,
+            passphrase=passphrase,
+        )
+        response = self.client.send(payload)
+
+        return response.result
+
+    def change_password(self, account, old_passphrase, new_passphrase):
+        payload = Request(
+            "account_changePassword",
+            account=account,
+            oldPassphrase=old_passphrase,
+            newPassphrase=new_passphrase,
+        )
+        response = self.client.send(payload)
+
+        return response.result
