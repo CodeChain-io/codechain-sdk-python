9acca0342d8b7c256b88ca80e368e6233ce84ab0
GNu <kgw6602@postech.ac.kr>
2019-12-31 14:26:58 +0900

    Add AssetTransferInput class

---
+++ codechain/sdk/core/assettransferinput.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+from typing import Union
+
+from .assetoutpoint import AssetOutPoint
+
+
+@dataclass
+class Timelock:
+    lock_type: str
+    value: int
+
+
+class AssetTransferInput:
+    def __init__(
+        self,
+        prev_out: AssetOutPoint,
+        timelock: Union[Timelock, None],
+        lock_script: Union[bytearray, bytes] = b"",
+        unlock_script: Union[bytearray, bytes] = b"",
+    ):
+        self.prev_out = prev_out
+        self.timelock = timelock
+        self.lock_script = lock_script
+        self.unlock_script = unlock_script
+
+    @staticmethod
+    def from_json(data):
+        return AssetTransferInput(
+            AssetOutPoint.from_json(data["prevOut"]),
+            data["timelock"],
+            bytes(data["lockScript"]),
+            bytes(data["unlockScript"]),
+        )
+
+    def to_encode_object(self):
+        return [
+            self.prev_out.to_encode_object(),
+            convert_timelock_to_encode_object(self.timelock),
+            self.lock_script,
+            self.unlock_script,
+        ]
+
+    def to_json(self):
+        return {
+            "prevOut": self.prev_out.to_json(),
+            "timelock": self.timelock,
+            "lockScript": list(self.lock_script),
+            "unlockScript": list(self.unlock_script),
+        }
+
+    def without_script(self):
+        return AssetTransferInput(self.prev_out, self.timelock, bytes(), bytes())
+
+
+def convert_timelock_to_encode_object(timelock: Union[Timelock, None]):
+    if timelock is None:
+        return []
+
+    lock_type = timelock.lock_type
+    value = timelock.value
+    if lock_type == "block":
+        type_encoded = 1
+    elif lock_type == "blockAge":
+        type_encoded = 2
+    elif lock_type == "time":
+        type_encoded = 3
+    elif lock_type == "timeAge":
+        type_encoded = 4
+    else:
+        raise ValueError(f"Unexpected timelock type: {lock_type}")
+
+    return [[type_encoded, value]]
